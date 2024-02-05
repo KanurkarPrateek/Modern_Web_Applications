96ef227b6afafb4c60db4eeb745e3d2ef664a257
Kanurkar Prateek <53488723+KanurkarPrateek@users.noreply.github.com>
2024-02-05 13:02:30 +0530

    added challenge

---
+++ HomeWork-Projects/Challenge/navigation.js
@@ -0,0 +1,9 @@
+// navigation.js
+
+function navigateTo(author) {
+    if (author === 'shubham') {
+        window.location.href = 'shubham.html';
+    } else if (author === 'pawan') {
+        window.location.href = 'pawan.html';
+    }
+}
